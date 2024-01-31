a4b50b1befc9dd7dfc770f88f41ceeba39c1d260
Angel402 <bolo402402@gmail.com>
2024-01-30 22:13:34 -0600

    commit to pull

---
+++ Assets/Scripts/Gameplay/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public abstract class EnemyBehaviour : MonoBehaviour
+    {
+        [SerializeField] private int health;
+
+        public abstract void Attack();
+        public abstract void GetDamage();
+    }
+
+    public class EnemiesSystem : MonoBehaviour
+    {
+        [Serializable]
+        public class Enemy
+        {
+            public EnemyBehaviour enemyBehaviour;
+            public float activationSeconds;
+        }
+
+        /*
+        private void Start()
+        {
+            StartCoroutine()
+        }
+        
+        private IEnumerator */
+    }
+}
