7258cef0049d81239bff3d81f16d9339163f5c96
Filip A <filiparg2000@gmail.com>
2024-02-02 02:15:35 -0800

    Shotgun
    Attached shotgun to character and added animations for it

---
+++ Assets/Scripts/Gameplay/GunController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    Animator m_animator;
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            m_animator.SetTrigger("Shoot");
+        }
+    }
+}
